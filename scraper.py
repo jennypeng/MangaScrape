c7cb51b731be98386de945ae085fb5bb8359240d
Jenny Peng <pengjenny@berkeley.edu>
2014-02-12 23:05:59 -0800

    now downloads all pages in chapter to pdf

--- scraper.py
+++ scraper.py
@@ -30,10 +30,17 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
 # User-Agent (this is cheating, ok?)
 # br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+'''
+Prompt for a series name and open the url for the series page.
+'''
 def prompt():
     series_name = raw_input("Series name: ").lower().strip().replace(" ", "_")
     url = "http://mangafox.me/manga/" + series_name + "/"
     openURL(url)
+
+'''
+Ask the users for a new request or to exit the program.
+'''
 def reprompt():
     print("[y] for new series request")
     print("[q] for exit")
@@ -45,28 +52,34 @@ def reprompt():
     else:
         print("Sorry, I couldn't understand you")
         reprompt()
-
-def getFiles(url):
-    page = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(page)
-    soup.prettify()
-    imageSRC = soup.find('img', {'id': "image"}).get('src')
-    # try to download image
+'''
+Download the image located at imageSRC, and save into \series_name\chapter[chapter_num]\. 
+'''
+def getImage(src, chapter_num, series_name, page_num):
+    #while not src == "": # we w=set tge src ti e=tge enoty string when mechanize fails to find a next page in the chapter
+        #counter = 1 # start off at page 1 
     try:
-        imgData = urllib2.urlopen(imageSRC).read()
-        fileName = "page3.jpg" #change later
+        imgData = urllib2.urlopen(src).read()
+        fileName = "page" + str(page_num) + ".jpg" # keep a running count of what page we're on 
         output = open(fileName, 'wb')
         output.write(imgData)
         output.close()
-        #os.rename('/' + fileName, '/chapter1/' + fileName)
-        print "image downloaded"
+        os.rename('/' + fileName, '/' + series_name + '/chapter' + str(chapter_num) + "/" + fileName)
+        #counter += 1
+        #get the next image
+        #src = mechanize
+
+        #print "image downloaded"
     except:
         raise
-    # try to convert to pdf
+    '''
+Convert the images to a pdf.
+'''
+def convertPDF(chapter_num, seriess_name):
     try:
         n = 0
-        for dirpath, dirnames, filenames in os.walk('\'):
-            PdfOutputFileName = "chapter2.pdf"
+        for dirpath, dirnames, filenames in os.walk('\\' + series_name + '\\chapter' + str(chapter_num) + "\\"): # the downloaded files are going to be in chapter directory 
+            PdfOutputFileName = "chapter" + str(chapter_num) + ".pdf"
             c = canvas.Canvas(PdfOutputFileName)
             if n > 0:
                 for filename in filenames:
@@ -84,6 +97,25 @@ def getFiles(url):
             print "PDF created" + PdfOutputFileName
     except:
         raise
+
+def getFiles(url, chapter_num):
+    page = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(page)
+    soup.prettify()
+    imageSRC = soup.find('img', {'id': "image"}).get('src')
+    # try to download image
+    counter = 1
+    while not nextPage == "javascript:void(0)":
+        getImage(imageSRC, chapter_num, counter)
+        nextPage = soup.find('a', {'onclick': "return enlarge()"}).get('href')
+        page = urllib2.urlopen(baseurl + nextPage)
+        soup = BeautifulSoup(page)
+        soup.prettify()
+        imageSRC = soup.find('img', {'id': "image"}).get('src')
+        counter += 1
+    # try to convert all images to pdf
+    convertPDF(chapter_num)
+    
 
 
 
@@ -103,13 +135,14 @@ def openURL(url):
         br.submit()
         #print(br.response().read())
     else: # does not work for half chapters at the moment
-        chapter_num = raw_input("Chapter number: ").zfill(3)
+        chapter_num = raw_input("Chapter number: ")
+        zero_pad_num = chapter_num.zfill(3)
         for chapter in soup.find_all('a', {'class': "tips"}):
             chapterURL = chapter.get('href')
             query = 'c' + chapter_num
             if query in chapterURL:
                 print("found query at " + chapterURL)
-                getFiles(chapterURL)
+                getFiles(chapterURL, chapter_num, "test1")
                 break
                 
 
